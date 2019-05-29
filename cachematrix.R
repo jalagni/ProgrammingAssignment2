0227ecb2078b8b99cae22b143ac0d8a353e0f341
Mohan Kumar <mohan.wing@hotmail.com>
2019-05-30 00:59:16 +0400

    mean_integrated

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cache <- NULL
+  
+  getCache <- function() cache
+  setCache <- function(value = NULL)  cache <<- value 
+  
+  setData <- function(m = NULL) {
+    cache <<- NULL
+    x <<- m  
+  }
+  getData <- function()  x
+  
+  list(getData = getData  , setData = setData,
+       getCache = getCache, setCache = setCache )
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  if (!is.null(x$getCache())){
+    message("Retrieving Cached:")
+    return(x$getCache())
+  }  
+
+  
+  data <- x$getData()
+  m <- mean(data)
+  
+  x$setCache(m)
+  m
+  
 }
+
